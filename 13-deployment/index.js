6e893dd211093dfad2c94363130d13a93d67602e
boram <sahul0804@gmail.com>
2018-11-06 10:05:22 +0900

    section 15 준비

---
+++ 13-deployment/index.js
@@ -0,0 +1,16 @@
+const winston = require('winston');
+const express = require('express');
+const app = express();
+
+require('./startup/config')();
+require('./startup/logging')();
+require('./startup/db')();
+require('./startup/routes')(app);
+require('./startup/validation')();
+
+const port = process.env.PORT || 3000;
+if (process.env.NODE_ENV !== 'test') {
+    app.listen(port, () => winston.info(`Listening on port ${port}...`));
+}
+
+module.exports = app;
---
+++ 13-deployment/models/rental.js
@@ -0,0 +1,88 @@
+const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
+const moment = require('moment');
+const mongoose = require('mongoose');
+
+const rentalSchema = new mongoose.Schema({
+  customer: {
+    type: new mongoose.Schema({
+      name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50,
+      },
+      isGold: {
+        type: Boolean,
+        default: false,
+      },
+      phone: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50,
+      }
+    }),
+    required: true,
+  },
+  movie: {
+    type: new mongoose.Schema({
+      title: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 5,
+        maxlength: 255
+      },
+      dailyRentalRate: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 255
+      }
+    }),
+    required: true,
+  },
+  dateOut: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  dateReturned: {
+    type: Date,
+  },
+  rentalFee: {
+    type: Number,
+    min: 0,
+  }
+});
+rentalSchema.statics.lookup = function (customerId, movieId) {
+  return this.findOne({
+    'customer._id': customerId,
+    'movie._id': movieId,
+  });
+}
+
+rentalSchema.methods.return = function () {
+  this.dateReturned = new Date();
+  const rentalDays = moment().diff(this.dateOut, 'days');
+  this.rentalFee = rentalDays * this.movie.dailyRentalRate;
+
+  // const rentalDays = Math.round((rental.dateReturned - rental.dateOut) / (1000*3600*24)); // 얘도 작동함
+  // rental.rentalFee = rental.movie.dailyRentalRate * rentalDays;
+}
+
+const Rental = mongoose.model('rental', rentalSchema);
+
+function validateRental(rental) {
+  const schema = {
+    customerId: Joi.objectId().required(),
+    movieId: Joi.objectId().required(),
+  };
+
+  return Joi.validate(rental, schema);
+}
+
+exports.rentalSchema = rentalSchema;
+exports.Rental = Rental;
+exports.validateRental = validateRental;
